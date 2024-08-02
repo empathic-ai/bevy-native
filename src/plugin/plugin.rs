ddb15e39712da1d9a593f05d9a4a7e3be2b62c0a
Catchawink <threeeasywords@gmail.com>
2024-08-01 21:12:34 -0400

    Updating files.

--- src/plugin/plugin.rs
+++ src/plugin/plugin.rs
@@ -19,7 +19,7 @@ impl Plugin for BevyNative {
         .register_component_as::<dyn Bindable, UsageView>()
         .register_component_as::<dyn Bindable, InteractState>()
         .register_component_as::<dyn Bindable, SearchInput>()
-        .add_plugins((bevy::MinimalPlugins, bevy::hierarchy::HierarchyPlugin))
+        //.add_plugins((bevy::MinimalPlugins, bevy::hierarchy::HierarchyPlugin))
         .insert_resource(bevy_builder::prelude::Bindings { ..default() } )
         //.insert_resource(client)
         .add_event::<RouteChange>()
